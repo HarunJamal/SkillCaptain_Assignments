5a1c1d2f5809ee2ac45e9b1d13a3d7ccef88f712
unknown <harun>
2023-08-31 15:20:16 +0530

    day four

--- Day4/LargerOfTwo.java
+++ Day4/LargerOfTwo.java
@@ -4,20 +4,21 @@ public class LargerOfTwo {
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
-
         System.out.println("Enter 2 numbers");
         int a = input.nextInt();
         int b = input.nextInt();
 
         if (a>b){
-          System.out.println(a);
+            System.out.println("The larger number is "+a);
         }
         else if (b>a){
-            System.out.println(b);
+            System.out.println("The larger number is "+b);
         }
         else{
             System.out.println("both are equal/invalid input");
         }
+
+        input.close();
 
     }
 }
