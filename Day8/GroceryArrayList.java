1f6db65dc672d62ab22da87fd2974284c69ccfb1
unknown <harun>
2023-09-05 00:05:53 +0530

    cooking

--- Day8/GroceryArrayList.java
+++ Day8/GroceryArrayList.java
@@ -1,6 +1,45 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class GroceryArrayList {
-    
+    public static void main(String[] args) {
+        List<String> groceries = new ArrayList<>();
+        char op;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Grocery list");
+        System.out.println("-------------");
+        do{
+        int x;
+        System.out.println("Choose from the following operations:\n1. add item\n2. remove an item\n3. Print Grocery list\n4. Item Check\n5. Clear grocery list \n6. Terminate");
+        switch (x) {
+            case 1:
+                Sca
+                break;
+            case 2:
+                
+                break;
+            case 3:
+            {
+                System.out.println("Current items in grocery list:");
+                for (String a : groceries){
+                    System.out.println(a);
+                }
+            }
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                
+                break;
+        
+            default:
+                break;
+        }  
+        }
+        while (op !=)
+        
+    }
 }
