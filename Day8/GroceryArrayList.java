6221de38ae75df804ba9274bd7f213db97b377b7
unknown <harun>
2023-09-06 14:02:04 +0530

    Day 8 bit late

--- Day8/GroceryArrayList.java
+++ Day8/GroceryArrayList.java
@@ -1,61 +1,94 @@
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class GroceryArrayList {
+    public static ArrayList<String> groceries = new ArrayList<>();
+
     public static void main(String[] args) {
-        List<String> groceries = new ArrayList<>();
         Scanner input = new Scanner(System.in);
 
         System.out.println("Grocery list");
         System.out.println("-------------");
-        do{
+        while(true){
         System.out.println("Choose from the following operations:\n1. add item\n2. remove an item\n3. Print Grocery list\n4. Item Check\n5. Clear grocery list \n6. Terminate");
         int x = input.nextInt();
+        input.nextLine();
 
         switch (x) {
             case 1:
-            System.out.println("Enter item to add:");
-            String thenga = input.nextLine();
-            groceries.add(thenga);
+            addItem(input);
                 break;
 
             case 2:
-            System.out.println("Enter item to remove:");
+            removeItem(input);
+                break;
+
+            case 3:
+            printGrocerylist();
+                break;
+
+            case 4:
+            checkItem(input);
+                break;
+
+            case 5:
+                clearGrocery();
+                break;
+            
+            case 6:
+            System.out.println("grocery list terminated");
+                System.exit(0);
+
+            default:
+            System.out.println("Invalid operation (press number between 1 - 6)");
+            break;
+            
+            }
+            
+        }
+
+    }
+
+    private static void clearGrocery(){
+         System.out.println("Clearing entire list....");
+         groceries.clear();
+    }
+    
+    private static void addItem(Scanner input){
+        System.out.println("Enter item to add:");
+        String thenga = input.nextLine();
+        groceries.add(thenga);
+    }
+
+    private static void removeItem(Scanner input){
+        System.out.println("Enter item to remove:");
             String manga = input.nextLine();
             for (String chakka : groceries){
                 if (chakka == manga){
                     groceries.remove(chakka);
                 }
-            }
-                break;
-
-            case 3:
-            {
-                System.out.println("Current items in grocery list:");
-                for (String a : groceries){
-                    System.out.println(a);
-                }
-            }
-                break;
-
-            case 4:
-            System.out.println("Enter item to see if its there or not:");
-            String baingan = input.nextLine();
-            System.out.println(groceries.contains(baingan));
-                break;
-
-            case 5:
-                System.out.println("Clearing entire list....");
-                groceries.clear();
-                break;
-        
-            default:
-            System.out.println("Invalid operation");
-                break;
-            }  
         }
-        while (op == 'Y');
-        
     }
+
+    private static void printGrocerylist(){
+        
+        System.out.println("Current items in grocery list:");
+        for (String a : groceries){
+            System.out.println(a);
+        }
+    }
+
+    private static void checkItem(Scanner input){
+        System.out.println("Enter item to see if its there or not:");
+            String baingan = input.nextLine();
+            boolean dhaniya = groceries.contains(baingan);
+            if (dhaniya){
+                System.out.println(baingan+" is there in grocery list");
+            }
+            else{
+                System.out.println(baingan+" is not there in grocery list");
+            }
+    }
+
+
 }
