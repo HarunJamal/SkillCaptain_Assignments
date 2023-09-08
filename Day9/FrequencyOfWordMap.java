646237386fbef703a2cb85aa1ac08ddde560dc30
unknown <harun>
2023-09-08 12:19:01 +0530

    Day 9

---
+++ Day9/FrequencyOfWordMap.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class FrequencyOfWordMap{
+    
+    public static void main(String[] args) {
+        Map <String, Integer> WordCounter = new HashMap<>();
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("Enter the paragraph:");
+        String para = input.nextLine();
+        System.out.println("nice paragraph");
+
+        input.close();
+        
+        String[] words = para.split(" ");
+        for (String word : words){
+            if (WordCounter.containsKey(word)){
+                int curval = WordCounter.get(word);
+                WordCounter.put(word, curval + 1);
+            }
+            else{
+                WordCounter.put(word, 1);
+            }
+        }
+
+        System.out.println("Here is the list of words and their occurences:");
+        System.out.println("=================================================");
+        System.out.println("Word\t\tOccurence");
+        System.out.println("------------------------------------");
+        for (Map.Entry<String, Integer> entry : WordCounter.entrySet()) {
+            System.out.println(entry.getKey() + "\t\t " + entry.getValue());
+        }
+
+    }
+}
